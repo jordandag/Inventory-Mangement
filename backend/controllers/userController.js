8022c3a8d6d557048aeac40cc1f7783e727384bc
Dagemawi Bmeskel <dive.dag.20219@gmail.com>
2025-07-29 16:03:55 +0300

    added a loginUser controller with all the jwt token and res.cookie and res.json

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,7 @@
 const asyncHandler = require("express-async-handler");
 const User = require("../models/userModel");
 const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
 
 // Generate Token
 const generateToken = (id) => {
@@ -38,19 +39,20 @@ const registerUser = asyncHandler(async (req, res) => {
   });
 
   if (user) {
+    const { _id, name, email, photo, phone, bio } = user;
+
     // Generate Token
-    const token = generateToken(user._id);
+    const token = generateToken(_id);
 
     // Send HTTP-only cookie
     res.cookie("token", token, {
       path: "/",
       httpOnly: true,
-      expires: new Date(Date.now() + 1000 * 86400), // 1 day
+      expires: new Date(Date.now() + 1000 * 60 * 60 * 24), // 1 day
       sameSite: "none",
       secure: true,
     });
 
-    const { _id, name, email, photo, phone, bio } = user;
     res.status(201).json({
       _id,
       name,
@@ -66,6 +68,57 @@ const registerUser = asyncHandler(async (req, res) => {
   }
 });
 
+// Login User
+const loginUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+
+  // Validate Request
+  if (!email || !password) {
+    res.status(400);
+    throw new Error("Please add email and password");
+  }
+
+  // Check if user exists
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    res.status(400);
+    throw new Error("User not found, please signup");
+  }
+
+  // User exists, check if password is correct
+  const passwordIsCorrect = await bcrypt.compare(password, user.password);
+
+  if (user && passwordIsCorrect) {
+    const { _id, name, email, photo, phone, bio } = user;
+
+    // Generate Token
+    const token = generateToken(_id);
+
+    //  Send HTTP-only cookie
+    res.cookie("token", token, {
+      path: "/",
+      httpOnly: true,
+      expires: new Date(Date.now() + 1000 * 60 * 60 * 24), // 1 day
+      sameSite: "none",
+      secure: true,
+    });
+    res.status(200).json({
+      _id,
+      name,
+      email,
+      photo,
+      phone,
+      bio,
+      token,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid email or password");
+  }
+});
+
 module.exports = {
   registerUser,
+  loginUser,
 };
