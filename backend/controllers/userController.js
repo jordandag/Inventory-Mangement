12e9fa891bad23f57f56211c5d3a34e5de4aba0a
Dagemawi Bmeskel <dive.dag.20219@gmail.com>
2025-08-06 09:06:54 +0300

    created user logout route and also created authMiddleware.js with protect route

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -118,7 +118,27 @@ const loginUser = asyncHandler(async (req, res) => {
   }
 });
 
+// Logout User
+const logout = asyncHandler(async (req, res) => {
+  res.cookie("token", "", {
+    path: "/",
+    httpOnly: true,
+    expires: new Date(0),
+    sameSite: "none",
+    secure: true,
+  });
+
+  return res.status(200).json({ message: "Successfully Logged Out" });
+});
+
+// Get User Data
+const getUser = asyncHandler(async (req, res) => {
+  res.send("get user");
+});
+
 module.exports = {
   registerUser,
   loginUser,
+  logout,
+  getUser,
 };
